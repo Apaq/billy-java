533a75b4f9a00529da9c0e8c73063a03f8bac0bc
Michael Krog <mic@apaq.dk>
2016-11-18 23:32:49 +0100

    Adds a few more model classes

---
+++ src/main/java/com/previsto/model/Product.java
@@ -0,0 +1,51 @@
+package com.previsto.model;
+
+public class Product extends Entity {
+    private String name;
+    private String description;
+    private String account;
+    private String productNo;
+    private String suppliersProductNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getProductNo() {
+        return productNo;
+    }
+
+    public void setProductNo(String productNo) {
+        this.productNo = productNo;
+    }
+
+    public String getSuppliersProductNo() {
+        return suppliersProductNo;
+    }
+
+    public void setSuppliersProductNo(String suppliersProductNo) {
+        this.suppliersProductNo = suppliersProductNo;
+    }
+    
+    
+}
